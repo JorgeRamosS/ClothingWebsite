3edf70566de43d3b54f06ca1412b3556d98a45be
JorgeERamosS <golub0432@yahoo.com>
2022-08-26 14:09:59 -0300

    products section

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,33 @@ window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER PRODUCTS ===============*/
 
+let swiperProducts = new Swiper(".products__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      spaceBetween: 72,
+    },
+  },
+});
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
